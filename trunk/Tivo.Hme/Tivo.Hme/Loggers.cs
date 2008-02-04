35a5719a100ca5e82437020fe61c9ad1dcf2e364
josh.cooley@gmail.com <josh.cooley@gmail.com@22cada5a-4744-0410-8c1b-0bff22da7ea3>
2008-02-04 00:12:14 +0000

    Added support for custom icons

--- trunk/Tivo.Hme/Tivo.Hme/Loggers.cs
+++ trunk/Tivo.Hme/Tivo.Hme/Loggers.cs
@@ -49,4 +49,31 @@ namespace Tivo.Hme
             Write(TraceEventType.Verbose, data);
         }
     }
+
+    internal static class StatusLog
+    {
+        private static TraceSource _log = new TraceSource("ApplicationStatus");
+        private const int messageTraceId = 1;
+        private const int exceptionTraceId = 2;
+
+        public static void Write(TraceEventType traceType, string message)
+        {
+            _log.TraceData(traceType, messageTraceId, message);
+        }
+
+        public static void Write(string message)
+        {
+            Write(TraceEventType.Verbose, message);
+        }
+
+        public static void Write(TraceEventType traceType, Exception exception)
+        {
+            _log.TraceData(traceType, exceptionTraceId, exception);
+        }
+
+        public static void Write(Exception exception)
+        {
+            Write(TraceEventType.Verbose, exception);
+        }
+    }
 }
