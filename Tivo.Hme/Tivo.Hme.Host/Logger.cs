6fd651ab145ac26ab01209dbcea21cf83352d43d
josh.cooley@gmail.com <josh.cooley@gmail.com@22cada5a-4744-0410-8c1b-0bff22da7ea3>
2008-02-13 22:28:28 +0000

    Add logging for server

---
+++ Tivo.Hme/Tivo.Hme.Host/Logger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace Tivo.Hme.Host
+{
+    internal static class ServerLog
+    {
+        private static TraceSource _log = new TraceSource("ServerStatus");
+        private const int messageTraceId = 1;
+        private const int exceptionTraceId = 2;
+
+        public static void Write(TraceEventType traceType, string message)
+        {
+            _log.TraceData(traceType, messageTraceId, message);
+        }
+
+        public static void Write(string message)
+        {
+            Write(TraceEventType.Verbose, message);
+        }
+
+        public static void Write(TraceEventType traceType, Exception exception)
+        {
+            _log.TraceData(traceType, exceptionTraceId, exception);
+        }
+
+        public static void Write(Exception exception)
+        {
+            Write(TraceEventType.Verbose, exception);
+        }
+    }
+}
