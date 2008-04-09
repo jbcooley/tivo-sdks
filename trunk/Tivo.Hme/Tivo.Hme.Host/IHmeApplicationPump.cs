8c3bc33ef30ad904f7f3ffd46022c9e5e3371cf8
josh.cooley <josh.cooley@22cada5a-4744-0410-8c1b-0bff22da7ea3>
2008-04-09 13:03:31 +0000

    Added missing files.  Icon.png for default icon and IHmeApplicationPump for getting the Has project going

---
+++ trunk/Tivo.Hme/Tivo.Hme.Host/IHmeApplicationPump.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tivo.Hme.Host
+{
+    public interface IHmeApplicationPump
+    {
+        void AddHmeConnection(HmeConnection connection);
+    }
+}
