9035e46654078bca78567e6913f848587be70d0b
josh.cooley <josh.cooley@22cada5a-4744-0410-8c1b-0bff22da7ea3>
2008-04-25 02:04:11 +0000

    Fix build so pipeline builder isn't required.

--- Tivo.Hme/Tivo.Has.Contracts/IHmeApplicationDriverContract.cs
+++ Tivo.Hme/Tivo.Has.Contracts/IHmeApplicationDriverContract.cs
@@ -2,12 +2,14 @@
 using System.IO;
 using System.AddIn.Contract;
 using System.AddIn.Pipeline;
+#if PIPELINE_BUILDER
 using PipelineHints;
 
 [assembly: SegmentAssemblyName(PipelineSegment.AddInSideAdapter, "Tivo.Has.AddInSideAdapters")]
 [assembly: SegmentAssemblyName(PipelineSegment.AddInView, "Tivo.Has.AddInView")]
 [assembly: SegmentAssemblyName(PipelineSegment.HostSideAdapter, "Tivo.Has.HostSideAdapter")]
 [assembly: SegmentAssemblyName(PipelineSegment.HostView, "Tivo.Has.HostView")]
+#endif
 
 namespace Tivo.Has.Contracts
 {
@@ -17,20 +19,29 @@ namespace Tivo.Has.Contracts
         IHmeConnectionContract CreateHmeConnection(IHmeApplicationIdentityContract identity, Stream inputStream, Stream outputStream);
         void HandleEventsAsync(IHmeConnectionContract connection);
         void RunOneAsync(IHmeConnectionContract connection);
-
+        
+#if PIPELINE_BUILDER
         [EventAdd("ApplicationEnded")]
+#endif
         void ApplicationEndedEventAdd(IApplicationEndedEventHandler handler);
+
+#if PIPELINE_BUILDER
         [EventRemove("ApplicationEnded")]
+#endif
         void ApplicationEndedEventRemove(IApplicationEndedEventHandler handler);
     }
-
+    
+#if PIPELINE_BUILDER
     [EventHandler]
+#endif
     public interface IApplicationEndedEventHandler : IContract
     {
         void Handler(IApplicationEndedEventArgs args);
     }
-
+    
+#if PIPELINE_BUILDER
     [EventArgs]
+#endif
     public interface IApplicationEndedEventArgs : IContract
     {
     }
