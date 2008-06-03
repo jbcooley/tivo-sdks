274178a5de5ec52519abede281c075910ff6f533
josh.cooley <josh.cooley@22cada5a-4744-0410-8c1b-0bff22da7ea3>
2008-06-03 01:02:50 +0000

    mostly changes to the pipeline because streams and wait handles don't work in add-ins

--- trunk/Tivo.Hme/Tivo.Has/HmeApplicationService.cs
+++ trunk/Tivo.Hme/Tivo.Has/HmeApplicationService.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
 using System.Linq;
+using System.ServiceModel;
 using System.ServiceProcess;
-using System.Text;
+using System.Threading;
 
 namespace Tivo.Has
 {
     public partial class HmeApplicationService : ServiceBase
     {
+        private ServiceHost _configurationServiceHost = new ServiceHost(typeof(Configuration.HasConfigurationService));
+        private HmeServersController _controller = new HmeServersController();
+
         public HmeApplicationService()
         {
             InitializeComponent();
@@ -18,12 +19,32 @@ namespace Tivo.Has
 
         protected override void OnStart(string[] args)
         {
-            // TODO: start a new thread
+            ThreadPool.QueueUserWorkItem(StartApplications);
+            // this opens the configuration host
+            // with a thread to respond to requests
+            _configurationServiceHost.Open();
         }
 
         protected override void OnStop()
         {
-            // TODO: abort worker thread
+            lock (_controller)
+            {
+                _controller.StopAllServers();
+                _controller.Clear();
+            }
+            _configurationServiceHost.Close();
+        }
+
+        public void StartApplications(object unused)
+        {
+            if (Properties.Settings.Default.ApplicationDirectories == null)
+                Properties.Settings.Default.ApplicationDirectories = new System.Collections.Specialized.StringCollection();
+
+            string[] applicationDirectories = Properties.Settings.Default.ApplicationDirectories.Cast<string>().ToArray();
+            lock (_controller)
+            {
+                _controller.LoadApplications(server => server.Start(), applicationDirectories);
+            }
         }
     }
 }
