6afcc9f46e85d8e2b4f9eaa435fa21c3cf9d8dc4
josh.cooley <josh.cooley@22cada5a-4744-0410-8c1b-0bff22da7ea3>
2008-06-03 00:36:03 +0000

    Updated Contracts and add-in pipeline

---
+++ trunk/Tivo.Hme/Tivo.Has/Configuration/IHasConfigurationService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Tivo.Has.Configuration
+{
+    // NOTE: If you change the interface name "IService1" here, you must also update the reference to "IService1" in App.config.
+    [ServiceContract]
+    public interface IHasConfigurationService
+    {
+        [OperationContract]
+        string[] GetApplicationDirectories();
+        [OperationContract]
+        void AddApplicationDirectory(string directory);
+        [OperationContract]
+        void RemoveApplicationDirectory(string directory);
+        [OperationContract]
+        void StopAllApplications();
+        [OperationContract]
+        void StopApplication(string applicationName);
+        [OperationContract]
+        void StopApplications(string[] applicationNames);
+        [OperationContract]
+        void StartApplication(string applicationName);
+        [OperationContract]
+        void StartApplications(string[] applicationNames);
+        [OperationContract]
+        string[] GetApplicationNames(string directory);
+    }
+}
