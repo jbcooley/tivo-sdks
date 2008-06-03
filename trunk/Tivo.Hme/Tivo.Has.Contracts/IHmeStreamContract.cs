0cdb49a5c367f2728420431bbeb1ab07c3a7fb30
josh.cooley <josh.cooley@22cada5a-4744-0410-8c1b-0bff22da7ea3>
2008-06-03 00:36:59 +0000

    Updated Contracts and add-in pipeline

---
+++ trunk/Tivo.Hme/Tivo.Has.Contracts/IHmeStreamContract.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.AddIn.Contract;
+
+namespace Tivo.Has.Contracts
+{
+    public interface IHmeStreamContract : IContract
+    {
+        bool CanRead { get; }
+        bool CanWrite { get; }
+        bool CanTimeout { get; }
+        int ReadTimeout { get; set; }
+        int WriteTimeout { get; set; }
+
+        int Read(byte[] buffer, int offset, int count);
+        int ReadByte();
+        IHmeAsyncResultContract BeginRead(byte[] buffer, int offset, int count, IHmeAsyncCallbackContract callback);
+        int EndRead(IHmeAsyncResultContract asyncResult);
+        void Write(byte[] buffer, int offset, int count);
+        void WriteByte(byte value);
+        IHmeAsyncResultContract BeginWrite(byte[] buffer, int offset, int count, IHmeAsyncCallbackContract callback);
+        void EndWrite(IHmeAsyncResultContract asyncResult);
+        void Close();
+        void Flush();
+    }
+
+    public interface IHmeAsyncResultContract : IContract
+    {
+        bool CompletedSynchronously { get; }
+        bool IsCompleted { get; }
+    }
+
+    public interface IHmeAsyncCallbackContract : IContract
+    {
+        void AsyncCallback(IHmeAsyncResultContract asyncResult);
+    }
+}
