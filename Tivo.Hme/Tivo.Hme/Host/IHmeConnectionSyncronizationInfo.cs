ee0f9a1788ec12b6bdebc3b0209519a0788a7337
josh.cooley <josh.cooley@22cada5a-4744-0410-8c1b-0bff22da7ea3>
2008-06-03 01:10:38 +0000

    refactored to support syncronization outside of appdomain and better handling of application end

---
+++ Tivo.Hme/Tivo.Hme/Host/IHmeConnectionSyncronizationInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tivo.Hme.Host
+{
+    public interface IHmeConnectionSyncronizationInfo
+    {
+        string EventReceivedName { get; }
+        string CommandReceivedName { get; }
+    }
+}
