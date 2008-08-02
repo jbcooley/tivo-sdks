84b1d65fbbf3314e47dc435882ce48d7bf351e69
josh.cooley <josh.cooley@22cada5a-4744-0410-8c1b-0bff22da7ea3>
2008-08-02 03:19:32 +0000

    support installing as a service with installutil

---
+++ Tivo.Hme/Tivo.Has/HasServiceInstaller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration.Install;
+using System.ServiceProcess;
+using System.ComponentModel;
+
+
+
+namespace Tivo.Has
+{
+    [RunInstaller(true)]
+    public class HasServiceInstaller : Installer
+    {
+        private ServiceInstaller _serviceInstaller = new ServiceInstaller();
+        private ServiceProcessInstaller _processInstaller = new ServiceProcessInstaller();
+
+        public HasServiceInstaller()
+        {
+            _processInstaller.Account = ServiceAccount.LocalService;
+            _serviceInstaller.StartType = ServiceStartMode.Automatic;
+            _serviceInstaller.ServiceName = "HME Application Service";
+
+            Installers.Add(_serviceInstaller);
+            Installers.Add(_processInstaller);
+        }
+    }
+}
