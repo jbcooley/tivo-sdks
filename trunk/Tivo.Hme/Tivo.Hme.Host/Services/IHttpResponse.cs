336cf3c6bb5fae30356103dde690dfc8d5d195a9
josh.cooley <josh.cooley@22cada5a-4744-0410-8c1b-0bff22da7ea3>
2008-10-15 01:28:35 +0000

    added hme host services

---
+++ trunk/Tivo.Hme/Tivo.Hme.Host/Services/IHttpResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+
+namespace Tivo.Hme.Host.Services
+{
+    public interface IHttpResponse
+    {
+        Stream GetResponseStream(int statusCode, NameValueCollection headers);
+    }
+}
