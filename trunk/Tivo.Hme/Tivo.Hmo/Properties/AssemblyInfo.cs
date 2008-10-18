992c6e9abad13ab559b65ba524fb349eb2bfe056
josh.cooley <josh.cooley@22cada5a-4744-0410-8c1b-0bff22da7ea3>
2008-10-18 02:37:18 +0000

    Fix Copyright

--- trunk/Tivo.Hme/Tivo.Hmo/Properties/AssemblyInfo.cs
+++ trunk/Tivo.Hme/Tivo.Hmo/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Tivo.Hmo")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Tivo HMO library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyCompany("Josh Cooley")]
 [assembly: AssemblyProduct("Tivo.Hmo")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2007")]
+[assembly: AssemblyCopyright("Copyright © 2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
