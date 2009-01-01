c4cd30a2881751a2e15533e38ad3be47bdd944d8
josh.cooley <josh.cooley@22cada5a-4744-0410-8c1b-0bff22da7ea3>
2009-01-01 05:22:25 +0000

    Added XML comments

--- trunk/Tivo.Hme/Tivo.Hme/Host/IHmeConnectionSyncronizationInfo.cs
+++ trunk/Tivo.Hme/Tivo.Hme/Host/IHmeConnectionSyncronizationInfo.cs
@@ -4,9 +4,18 @@ using System.Text;
 
 namespace Tivo.Hme.Host
 {
+    /// <summary>
+    /// Provides access to the names of the connection events used in syncronization.
+    /// </summary>
     public interface IHmeConnectionSyncronizationInfo
     {
+        /// <summary>
+        /// The name of the event that is set when HME events are received.
+        /// </summary>
         string EventReceivedName { get; }
+        /// <summary>
+        /// The name of the event that is set when HME commands are ready to be sent.
+        /// </summary>
         string CommandReceivedName { get; }
     }
 }
