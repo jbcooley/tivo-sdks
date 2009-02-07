86556fd385a9f0f0e4ee333d38d5cbdf88c751cf
josh.cooley <josh.cooley@22cada5a-4744-0410-8c1b-0bff22da7ea3>
2009-02-07 01:52:52 +0000

    Used to indicate an application uses the host webserver to serve files based on the BaseUri

---
+++ trunk/Tivo.Hme/Tivo.Hme/Host/Services/UsesHostHttpServicesAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tivo.Hme.Host.Services
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public sealed class UsesHostHttpServicesAttribute : Attribute
+    {
+    }
+}
