d7e081a6ad63a44827b984bd4784121dc4a42445
Cooley, Joshua B <josh.cooley@hexagonsi.com>
2016-04-05 19:53:13 -0500

    Fix namespace to match the rest of the project

--- Tivo.Hme/Tivo.Hmo/TivoVideoDetails.cs
+++ Tivo.Hme/Tivo.Hmo/TivoVideoDetails.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
-namespace Cooley.Tivo.Hmo
+namespace Tivo.Hmo
 {
     public sealed class TivoVideoDetails : IEquatable<TivoVideoDetails>
     {
